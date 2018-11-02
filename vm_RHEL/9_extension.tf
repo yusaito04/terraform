26d133abd47fa82d448d004968cd1241e675354b
yu.saito04 <yu.saito04@g.softbank.co.jp>
2018-11-02 11:45:23 +0900

    ほげほげテスト

---
+++ vm_RHEL/9_extension.tf
@@ -0,0 +1,18 @@
+resource "azurerm_virtual_machine_extension" "TW" {
+  name                 = "${var.azurerm_virtual_machine_extension_name}"
+  location             = "${var.location}"
+  resource_group_name  = "${var.resource_group_name}"
+  virtual_machine_name = "${var.vm_name_001}"
+  publisher            = "Datadog.Agent"
+  type                 = "DatadogLinuxAgent"
+  type_handler_version = "0.4"
+
+  settings = <<SETTINGS
+  {
+    "settings": {
+        "commandToExecute": "DD_API_KEY=c059e5c26966c3c7aa8e99f2c10ec2fb"
+    }
+  }
+SETTINGS
+}
+
